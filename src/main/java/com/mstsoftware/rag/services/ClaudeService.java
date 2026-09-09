567500812b319535bfd91ec0480179f4d4744093
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-09 19:54:20 +0700

    feat: response struct claude

--- src/main/java/com/mstsoftware/rag/services/ClaudeService.java
+++ src/main/java/com/mstsoftware/rag/services/ClaudeService.java
@@ -1,18 +1,21 @@
 package com.mstsoftware.rag.services;
 
+import org.springframework.stereotype.Service;
+
 import com.anthropic.client.AnthropicClient;
 import com.anthropic.models.messages.Message;
 import com.anthropic.models.messages.MessageCreateParams;
 
 import lombok.AllArgsConstructor;
 
+@Service
 @AllArgsConstructor
 public class ClaudeService {
     private final AnthropicClient client;
     private static final String MODEL = "claude-sonnet-4-6";
     private static final Long MAX_TOKENS = 1000L;
 
-    String ask(String userMessage) {
+    public Message ask(String userMessage) {
         MessageCreateParams params = MessageCreateParams.builder()
                 .model(MODEL)
                 .maxTokens(MAX_TOKENS)
@@ -20,11 +23,11 @@ public class ClaudeService {
                 .build();
 
         Message response = client.messages().create(params);
-
-        return response.content().stream()
-                .filter(block -> block.type().toString().equals("text"))
-                .findFirst()
-                .map(block -> block.asText().text())
-                .orElse("");
+        return response;
+        // return response.content().stream()
+        // .filter(block -> block.type().toString().equals("text"))
+        // .findFirst()
+        // .map(block -> block.asText().text())
+        // .orElse("");
     }
 }
