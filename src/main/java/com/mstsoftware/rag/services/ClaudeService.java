27089739048ab9a0eeb2c54488d2e6859f08992b
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-18 07:23:03 +0700

    feat: chat streaming claude

--- src/main/java/com/mstsoftware/rag/services/ClaudeService.java
+++ src/main/java/com/mstsoftware/rag/services/ClaudeService.java
@@ -1,14 +1,18 @@
 package com.mstsoftware.rag.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.anthropic.client.AnthropicClient;
+import com.anthropic.core.http.StreamResponse;
+import com.anthropic.helpers.MessageAccumulator;
 import com.anthropic.models.messages.Message;
 import com.anthropic.models.messages.MessageCreateParams;
 import com.anthropic.models.messages.MessageParam;
+import com.anthropic.models.messages.RawMessageStreamEvent;
 import com.mstsoftware.rag.models.ConversationMessage;
 
 import lombok.AllArgsConstructor;
@@ -31,17 +35,35 @@ public class ClaudeService {
     }
 
     // hỏi AI dạng có ghi nhớ conversation và streaming
-    // public void chatStreming(List<ConversationMessage> messages, SseEmitter emitter) {
-    //     MessageCreateParams request = MessageCreateParams.builder()
-    //             .model(MODEL)
-    //             .maxTokens(1000L)
-    //             .messages(params)
-    //             .system(SYSTEM_PROMPT)
-    //             .temperature(TEMPERATURE)
-    //             .build();
-    // }
+    public void chatStreming(List<ConversationMessage> messages, SseEmitter emitter) {
+        MessageCreateParams request = buildRequest(messages);
+        MessageAccumulator accumulator = MessageAccumulator.create();
+        try (StreamResponse<RawMessageStreamEvent> streamResponse = client.messages().createStreaming(request)) {
+            streamResponse.stream()
+                    .peek(accumulator::accumulate)
+                    .flatMap(event -> event.contentBlockDelta().stream())
+                    .flatMap(deltaEvent -> deltaEvent.delta().text().stream())
+                    .forEach(textDelta -> {
+                        try {
+                            emitter.send(textDelta.text());
+                        } catch (IOException e) {
+                            emitter.completeWithError(e);
+                        }
+                    });
 
-    
+            // Lưu full response vào history
+            String fullText = accumulator.message().content().stream()
+                    .filter(block -> block.type().toString().equals("text"))
+                    .findFirst()
+                    .map(block -> block.asText().text())
+                    .orElse("");
+
+            addAssistantMessage(messages, fullText);
+            emitter.complete();
+        } catch (Exception ex) {
+            emitter.completeWithError(ex);
+        }
+    }
 
     // hỏi AI dạng có ghi nhớ conversation
     public String chat(List<ConversationMessage> messages) {
